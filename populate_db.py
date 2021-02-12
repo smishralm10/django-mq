debf04f49866a67f83fec9f1187b0d97aeb10db2
Shreyansh <smishralm10@gmail.com>
2021-02-12 19:01:14 +0530

    changed populate_db

--- populate_db.py
+++ populate_db.py
@@ -1,23 +1,20 @@
 import os
-import pandas as pd
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mq.settings")
 import django
+import random
 django.setup()
 from app.models import Employee
 
-df = pd.read_csv('Train.csv')
-cols = ['Gender', 'Education_Level', 'Relationship_Status', 'Unit', 'growth_rate', 'Attrition_rate']
-df = df[cols]
-data = 2000
+data = 1000000
 if len(Employee.objects.all()) > 1:
     Employee.objects.all().delete()
 for i in range(data):
     employee = Employee.objects.create()
-    employee.gender = str(df['Gender'][i])
-    employee.education_level = int(df['Education_Level'][i])
-    employee.relationship_status = str(df['Relationship_Status'][i])
-    employee.unit = str(df['Unit'][i])
-    employee.growth_rate = int(df['growth_rate'][i])
-    employee.attrition_rate = float(df['Attrition_rate'][i])
+    employee.gender = 'Male'
+    employee.education_level = random.randint(1, 10)
+    employee.relationship_status = 'Single'
+    employee.unit = 'IT'
+    employee.growth_rate = random.randint(1, 10)
+    employee.attrition_rate = float(0.0006)
     employee.save()
 
